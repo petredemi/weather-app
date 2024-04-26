b16c9c5a1f780efec9bd2018a3a06dd009fde9a1
petrudemian <petredemi@gmail.com>
2024-04-26 12:41:51 +0100

    set up initial project

---
+++ weather.js
@@ -0,0 +1,57 @@
+
+let temp = document.querySelector('#temp');
+const wind = document.querySelector('div.wind');
+const date = document.querySelector('div.date');
+const humidity = document.querySelector('div.humidity');
+const uvindex = document.querySelector('div.uv');
+const condition = document.querySelector('#condition');
+const img = document.querySelector('#status');
+const arrea = document.querySelector('div.arrea');
+let loc = 'london';
+const btnsearch = document.querySelector('#lookfor');
+let inputLocation = document.querySelector('#location');
+inputLocation.value = loc;
+
+// function getWeather(){
+//   fetch(`https://api.weatherapi.com/v1/current.json?key=69b808bac1c14633a67231851242404&q=${loc}`,{mode:'cors'})
+//       .then(function(response){
+//           return response.json();
+//       })
+//       .then(function(response){
+//           console.log(response);
+//           temp.textContent = response.current.temp_c + '  ℃';
+//           wind.textContent = response.current.wind_kph;
+//           date.textContent = response.location.localtime;
+//           humidity.textContent = response.current.humidity + ' %';
+//           uvindex.textContent = response.current.uv;
+//           condition.textContent = response.current.condition.text;
+//         //  img.src = response.current.condition.icon;
+//       })
+//
+//}
+async function getWeather(){
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=69b808bac1c14633a67231851242404&q=${loc}`,{mode:'cors'})
+        let getData = await response.json()
+   // await Promise.reject(new Error('woops'))
+         temp.textContent = getData.current.temp_c + '  ℃';
+         wind.textContent = getData.current.wind_kph;
+         date.textContent = getData.location.localtime
+         humidity.textContent = getData.current.humidity
+         uvindex.textContent = getData.current.uv
+         condition.textContent = getData.current.condition.text
+         arrea.textContent = getData.location.region
+  //      img.src = response.current.condition.icon;
+         console.log(getData);
+ }
+ 
+    getWeather()
+    btnsearch.addEventListener('click', (e) => {
+        if (inputLocation.value == ''){
+            inputLocation.value = 'london';
+        }
+        loc = inputLocation.value;
+        getWeather();
+    })
+    inputLocation.addEventListener('click', (e) => {
+        inputLocation.value = '';
+    })
